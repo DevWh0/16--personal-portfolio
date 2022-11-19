b42ab7a673524f29b9acc992a6259c5c86fd681c
DevWh0 <DevWho@outlook.fr>
2022-11-19 23:42:59 +0100

    javascript dev 2

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
-const tea = document.getElementsByClassName('RanMess');
-tea.onclick.innerHTML = 'Random Message : For this project, i will build a message generator program. Every time a user runs a program, they should get a new, randomized output. To make my program truly random, the message that it outputs should be made up of at least three different pieces of data.';
+let RanMess = document.getElementsByClassName('RanMess');
 
-const timer = document.getElementById('timer');
-const dateStart = date.parsec('2011-10-10');
-let DateNow = date.now();
+RanMess.onclick = function() {
+    RanMess.innerHTML = "Random Message : For this project, i will build a message generator program. Every time a user runs a program, they should get a new, randomized output. To make my program truly random, the message that it outputs should be made up of at least three different pieces of data.";
+};
 
-let DiffTime = dateNow - dateStart;
-timer.target.innerHTML = 'DiffTime';
+let timer = document.getElementById('timer');
+let dateStart = date.parsec('2011-10-10');
+let dateNow = date.now();
+
+let diffTime = dateNow - dateStart;
+timer.innerHTML = 'diffTime';
