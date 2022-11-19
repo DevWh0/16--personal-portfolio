b7fc684e16161956060f876183a5e5f4e11c10c3
DevWho <DevWho@outlook.fr>
2022-11-19 20:08:45 +0100

    javascript dev

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const tea = document.getElementsByClassName('RanMess');
+tea.onclick.innerHTML = 'Random Message : For this project, i will build a message generator program. Every time a user runs a program, they should get a new, randomized output. To make my program truly random, the message that it outputs should be made up of at least three different pieces of data.';
+
+const timer = document.getElementById('timer');
+const dateStart = date.parsec('2011-10-10');
+let DateNow = date.now();
+
+let DiffTime = dateNow - dateStart;
+timer.target.innerHTML = 'DiffTime';
